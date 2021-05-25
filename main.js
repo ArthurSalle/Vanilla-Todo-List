12d6811c9bd1174a5b94d12f6ccb5ba1625354e2
Arthur Sallé <arthursalle.pro@gmail.com>
2021-05-25 18:09:45 +0200

    create function add todo

---
+++ main.js
@@ -0,0 +1,48 @@
+const counterTodos = document.querySelector('.counter-todos');
+const formSubmit = document.querySelector('.form-todo');
+const list = document.querySelector('.todo-list');
+formSubmit.addEventListener('submit', newTodos);
+
+const todoArray = [];
+
+function newBtn(name) {
+  const btn = document.createElement('button');
+  btn.innerText = name;
+  return btn;
+}
+
+function newTodo(value) {
+  const todoContainer = document.createElement('div');
+  const todoInput = document.createElement('input');
+  todoInput.value = value;
+
+  const completedBtn = newBtn('Completed');
+  const deleteBtn = newBtn('Delete');
+
+  todoContainer.appendChild(todoInput);
+  todoContainer.appendChild(completedBtn);
+  todoContainer.appendChild(deleteBtn);
+
+  return todoContainer;
+}
+
+function newTodos(event) {
+  event.preventDefault();
+  console.log(event.target);
+  const inputValue = document.querySelector('.input-todo').value;
+
+  const todo = newTodo(inputValue);
+  list.appendChild(todo);
+
+  const todoObject = {
+    title: inputValue,
+    completed: false,
+    el: todo,
+  };
+
+  todoArray.push(todoObject);
+
+  counterTodos.innerText = todoArray.length;
+
+  event.target.reset();
+}
